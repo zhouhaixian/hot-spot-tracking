3863c56090995589f31bd84998cdf539f7016059
Zhou Haixian <2223540637@qq.com>
2022-05-18 22:18:11 +0800

    perf: 更好的启动逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { app } from "electron";
 import { createTray } from "./createTray";
 
-if (!app.requestSingleInstanceLock()) {
-  app.quit()
+if (app.requestSingleInstanceLock()) {
+  app.disableHardwareAcceleration();
+  app.whenReady().then(createTray);
+} else {
+  app.quit();
 }
-
-app.disableHardwareAcceleration();
-app.whenReady().then(createTray);
