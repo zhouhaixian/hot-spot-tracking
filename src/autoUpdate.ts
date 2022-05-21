608321a757c2a048fab142fcf9cdc95dbe0ea9fb
Zhou Haixian <2223540637@qq.com>
2022-05-21 15:15:09 +0800

    fix: 修复了自动更新的问题

--- src/autoUpdate.ts
+++ src/autoUpdate.ts
@@ -1,7 +1,11 @@
+import { app } from "electron";
 import { autoUpdater } from "electron-updater";
 import { scheduleJob } from "node-schedule";
 
 export function autoUpdate() {
+  const server = "https://hot-spot-tracking-update.vercel.app";
+  const url = `${server}/update/${process.platform}/${app.getVersion()}`
+  autoUpdater.setFeedURL(url)
   autoUpdater.checkForUpdates();
 
   const rule = "* 5 * * * *";
